9b8563bb1488d5e370b2b8e8a332c1644f39466f
kingsleylow0327 <kingsleylow94@gmail.com>
2024-03-23 01:44:17 +0800

    drop down not support

--- modal/activate_modal.py
+++ modal/activate_modal.py
@@ -8,14 +8,9 @@ class ActivateModal(discord.ui.Modal, title=TITLE):
         self.dbcon = dbcon
         super().__init__(title=TITLE, timeout=30)
 
-    package = [discord.SelectOption(label="2 Weeks", value="2 week"),
-              discord.SelectOption(label="3 Months", value="3 month"),
-              discord.SelectOption(label="6 Months", value="6 month"),
-              discord.SelectOption(label="1 Year", value="1 year")]
-    
     player_id = discord.ui.TextInput(label="User Id",placeholder="user id",style=discord.TextStyle.short)
     player_account_name = discord.ui.TextInput(label="Account Name",placeholder="user account name",style=discord.TextStyle.short)
-    expiry_date = discord.ui.Select(placeholder="Package Detail", options=package, min_values=1, max_values=1)
+    expiry_date = discord.ui.TextInput(label="Package",placeholder="2w(Default), 3m, 6m, 1y",style=discord.TextStyle.short)
 
     async def on_submit(self, interaction: Interaction):
         player_id = str(interaction.data.get("components")[0].get("components")[0].get("value"))
@@ -23,8 +18,18 @@ class ActivateModal(discord.ui.Modal, title=TITLE):
         user_account_name = user_account_name.lower()
         expiry_date = str(interaction.data.get("components")[2].get("components")[0].get("value"))
 
+        if expiry_date.lower() == "3m":
+            expiry_date = "3 month"
+        elif expiry_date.lower() == "6m":
+            expiry_date = "6 month"
+        elif expiry_date.lower() == "1y":
+            expiry_date = "1 year"
+        else:
+            expiry_date = "2 week"
+            return
+
         if not self.dbcon.check_user_exist_with_ref(player_id, user_account_name):
             self.dbcon.activate_user(player_id, user_account_name, expiry_date)
-            await interaction.response.send_message("User Activated", ephemeral=True)
+            await interaction.response.send_message(f"User Activated for {expiry_date}", ephemeral=True)
             return
         await interaction.response.send_message(f"User Id {player_id} has been activated before", ephemeral=True)
