aaee2a229bb7376d7c4961b559563441f54f1157
kingsleylow0327 <kingsleylow94@gmail.com>
2024-03-27 01:21:12 +0800

    try pagination

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # bot.py
+import asyncio
 import discord
 import message as ms
 from discord.ext import commands
@@ -141,6 +142,41 @@ async def status(interaction: discord.Interaction, id:str=None):
         embed.add_field(name=f"Damage Cost: `{trader_api.get('damage_cost')}%`", value="", inline=False)
         embed_list.append(embed)
         count += 1
-    await interaction.response.send_message(embed=embed_list, ephemeral=True)
+    
+    current = 0
+    buttons = [u"\u23EA", u"\u2B05", u"\u27A1", u"\u23E9"]
+    msg = await interaction.response.send_message(embed=embed_list[current], ephemeral=True)
+
+    for button in buttons:
+        await msg.add_reaction(button)
+        
+    while True:
+        try:
+            reaction, user = await bot.wait_for("reaction_add", check=lambda reaction, user: user == interaction.message.author and reaction.emoji in buttons, timeout=60.0)
+
+        except asyncio.TimeoutError:
+            return print("Time Out Error")
+
+        else:
+            previous_page = current
+            if reaction.emoji == u"\u23EA":
+                current = 0
+                
+            elif reaction.emoji == u"\u2B05":
+                if current > 0:
+                    current -= 1
+                    
+            elif reaction.emoji == u"\u27A1":
+                if current < len(embed_list)-1:
+                    current += 1
+
+            elif reaction.emoji == u"\u23E9":
+                current = len(embed_list)-1
+
+            for button in buttons:
+                await msg.remove_reaction(button, interaction.message.author)
+
+            if current != previous_page:
+                await msg.edit(embed=embed_list[current])
 
 bot.run(config.TOKEN)
