3c5d378ddcb31be9341496077809386b9e2d4a4c
kingsleylow0327 <kingsleylow94@gmail.com>
2024-04-05 01:53:38 +0800

    remove 1y, include 1m

--- modal/activate_modal.py
+++ modal/activate_modal.py
@@ -10,7 +10,7 @@ class ActivateModal(discord.ui.Modal, title=TITLE):
 
     player_id = discord.ui.TextInput(label="User Id",placeholder="user id",style=discord.TextStyle.short)
     player_account_name = discord.ui.TextInput(label="Account Name",placeholder="user account name",style=discord.TextStyle.short)
-    expiry_date = discord.ui.TextInput(label="Package",placeholder="2w(Default), 3m, 6m, 1y",style=discord.TextStyle.short, required=False)
+    expiry_date = discord.ui.TextInput(label="Package",placeholder="2w(Default), 1m, 3m, 6m",style=discord.TextStyle.short, required=False)
 
     async def on_submit(self, interaction: Interaction):
         player_id = str(interaction.data.get("components")[0].get("components")[0].get("value"))
@@ -18,12 +18,12 @@ class ActivateModal(discord.ui.Modal, title=TITLE):
         user_account_name = user_account_name.lower()
         expiry_date = str(interaction.data.get("components")[2].get("components")[0].get("value")).strip()
 
-        if expiry_date.lower() == "3m":
+        if expiry_date.lower() == "1m":
+            expiry_date = "1 month"
+        elif expiry_date.lower() == "3m":
             expiry_date = "3 month"
         elif expiry_date.lower() == "6m":
             expiry_date = "6 month"
-        elif expiry_date.lower() == "1y":
-            expiry_date = "1 year"
         else:
             expiry_date = "2 week"
 
